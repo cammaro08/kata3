b5607378f13500433d63c4399938f07929c7878a
TS <talha.i.sheikh@gmail.com>
2019-09-15 19:37:01 +0100

    first attempt

---
+++ src/main/java/Accumul.java
@@ -0,0 +1,44 @@
+public class Accumul {
+
+//    public static String accum(String s) {
+//
+//        int stringLength = s.length();
+//        int loopValue = 0;
+//        char stringInChar[] = s.toCharArray();
+//        String answer = String.valueOf(stringInChar[0]);
+//
+//        while (loopValue < stringLength){
+//
+//            for(int i =0; i<loopValue; i++){
+//                answer += String.valueOf(stringInChar[loopValue]); }
+//
+//            answer += "-";
+//            loopValue ++;
+//        }
+//
+//
+//        return answer;
+//    }
+
+    public static String accum(String s) {
+
+        StringBuilder builder = new StringBuilder();
+        char inputAsChar[] = s.toCharArray();
+        int loopValue = 0;
+
+        do {
+            String value = String.valueOf((inputAsChar[loopValue]));
+            builder.append(value.toUpperCase() + "-");
+            loopValue++;
+        }
+        while (loopValue < s.length());{
+
+            String value = String.valueOf((inputAsChar[loopValue]));
+            builder.append(value.toUpperCase() + "-");
+
+        }
+
+        return builder.toString();
+    }
+}
+
