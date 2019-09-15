e6f8a1744b04858d4fb81d6687fab224be587530
TS <talha.i.sheikh@gmail.com>
2019-09-15 19:35:32 +0100

    base version for code kata's with JUnit 4

---
+++ src/main/java/com/example/kata/kata3/Kata3Application.java
@@ -0,0 +1,13 @@
+package com.example.kata.kata3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata3Application.class, args);
+	}
+
+}
