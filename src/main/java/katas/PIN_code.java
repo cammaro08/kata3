c8f739354356c0dbf96a946094aea5ff9e91be1f
TS <talha.i.sheikh@gmail.com>
2019-09-16 20:59:46 +0100

    trying again

--- src/main/java/katas/PIN_code.java
+++ src/main/java/katas/PIN_code.java
@@ -4,9 +4,16 @@ public class PIN_code {
 
     public static boolean validatePin(String pin) {
 
-        if(pin.length() == 4 | pin.length() == 6){
-            for(char pinValue : pin.toCharArray())
-                return Character.isDigit(pinValue);
+//        if(pin.length() == 4 | pin.length() == 6) return pin.chars().allMatch(Character::isDigit);
+//        return false;
+
+        if (pin.length() == 4 || pin.length() == 6) {
+            for (int i = 0; i < pin.length(); i++) {
+                if (!Character.isDigit(pin.charAt(i))) {
+                    return false;
+                } else
+                    return true;
+            }
         }
         return false;
     }
